b1fc1f492b640bf148ad90a3241e8d893afd8829
cosmincalinov <cosmin.calinov@yahoo.com>
2024-04-29 18:52:52 +0300

    Etapa_6
    .

--- resurse/js/produse.js
+++ resurse/js/produse.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function(){
     }
 
     // document.getElementById("filtarre").addEventListener("click", function(){ })
-    document.getElementById("filtare").onclick = function() {
+    document.getElementById("filtrare").onclick = function() {
         var inpNume = document.getElementById("inp-nume").value.toLowerCase().trim();
 
         var radioCalorii = document.getElementsByName("gr_ad");
@@ -60,10 +60,69 @@ window.addEventListener("load", function(){
         document.getElementById("inp-pret").value=document.getElementById("inp-pret").min;
         document.getElementById("inp-categorie").value="toate";
         document.getElementById("i_rad4").checked=true;
-        var produse=document.getElementsByClassName("produs");
+        var produse = document.getElementsByClassName("produs");
         document.getElementById("infoRange").innerHTML="(0)";
         for (let prod of produse){
             prod.style.display="block";
+        }
+    }
+
+    function sorteaza(semn) {
+            var produse = document.getElementsByClassName("produs");
+            var v_produse = Array.from(produse);
+    
+            v_produse.sort(function(a,b) {
+                let pret_a = parseInt(a.getElementsByClassName("val-pret")[0].innerHTML);
+                let pret_b = parseInt(b.getElementsByClassName("val-pret")[0].innerHTML);
+    
+                if (pret_a == pret_b) {
+                    let nume_a = a.getElementsByClassName("val-nume")[0].innerHTML;
+                    let nume_b = b.getElementsByClassName("val-nume")[0].innerHTML;
+                    return semn*(nume_a.localeCompare(nume_b));
+                }
+                return semn*(pret_a - pret_b);
+            })
+    
+            for(let prod of v_produse) {
+                prod.parentNode.appendChild(prod);
+            }
+        }
+
+    document.getElementById("sortCrescNume").onclick = function() {
+        sorteaza(1);
+    }
+    document.getElementById("sortDescrescNume").onclick = function() {
+        sorteaza(-1);
+    }
+
+    window.onkeydown = function(e) {
+
+        if (e.key == "c" && e.altKey) {
+            var suma = 0;
+            var produse = document.getElementsByClassName("produs");
+
+            for(let produs of produse) {
+                var stil = getComputedStyle(produs);
+                if (stil.display != "none") {
+                    suma += parseFloat(produs.getElementsByClassName("val-pret")[0].innerHTML);
+                }
+            }
+
+            if (!document.getElementById("par_suma")) {
+                let p = document.createElement("p");
+                p.innerHTML = suma;
+                p.id = "par_suma";
+                container = document.getElementById("produse");
+                container.insertBefore(p, container.children[0]);
+
+                setTimeout(function (){
+                    var pgf = document.getElementById("par_suma");
+                    if (pgf) {
+                        pgf.remove();
+                    }
+                }, 2000);
+            }
+            
         }
     }
 })
